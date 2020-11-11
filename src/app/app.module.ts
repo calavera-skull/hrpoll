0f99f8c522ca2b02aa07bf7b54b04e4a1b30e097
calavera-skull <72505002+calavera-skull@users.noreply.github.com>
2020-11-11 16:05:50 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './source/components/questions/questions.component';
+import { QuestionItemComponent } from './source/components/question-item/question-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionsComponent,
+    QuestionItemComponent,
   ],
   imports: [
     BrowserModule
